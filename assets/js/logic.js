53a4aa33555f411504fb90c286fbce22e47ebe4b
Luis Angon <luisangon@Luiss-MacBook-Pro.local>
2017-06-16 18:57:31 -0700

    onclick event

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,14 +10,8 @@
   firebase.initializeApp(config);
    
 var database = firebase.database();
-
-    // // var twoDimensionalArrayOfLetters = [
-    // //     ['a1', 'b1', 'c1', 'd1'],
-    // //     ['a2', 'b2', 'c2', 'd2'],
-    // //     ['a3', 'b3', 'c3', 'd3'],
-    // //     ['a4', 'b4', 'c4', 'd4']
-    // ];
- 	   var arrayObjects = [
+function renderBoard(){
+var arrayObjects = [
         [
             {col: 'a', hasShip: false},
             {col: 'b', hasShip: false},
@@ -25,62 +19,90 @@ var database = firebase.database();
         ],
         [
             {col: 'a', hasShip: false},
-            {col: 'b', hasShip: "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif"},
-            {col: 'c', hasShip: false}
-        ],
-        [
-            {col: 'a', hasShip: "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif"},
             {col: 'b', hasShip: false},
             {col: 'c', hasShip: false}
         ],
         [
-            {col: 'a', hasShip: "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif"},
+            {col: 'a', hasShip: false},
+            {col: 'b', hasShip: false},
+            {col: 'c', hasShip: false}
+        ],
+        [
+            {col: 'a', hasShip: false},
             {col: 'b', hasShip: false},
             {col: 'c', hasShip: false}
         ]
     ];
-
-database.ref().set(arrayObjects);    
-
-//    arrayObjects[0][0].hasShip = "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif";
+database.ref().set(arrayObjects);
 
 
 
 
-    var board="<table border=2>";
-for (var y=0; y<arrayObjects.length; y++ ) { // for each row
+
+
+
+
+var board="<table border=2>";
+
+for (var y=0; y<arrayObjects.length; y++ ) {        // for each row
     board += "<tr>";
     for (var x=0; x<arrayObjects[y].length; x++ ) { // for each clm
-    	if(arrayObjects[y][x].hasShip) {
-    		board += "<td data-col="+ x +">" + arrayObjects[y][x].col + "<img src='"+arrayObjects[y][x].hasShip+"'/></td>";
-    	} else {
-    		board += "<td>" + arrayObjects[y][x].col + " <img src='https://media3.giphy.com/media/xT0GqcCJJJH12hJvGM/giphy.gif' /></td>";
-    	}
-        
-// EVERY TIME THE USER CLICKS ON AN IMAGE, STORE THE VALUE ON A VARIABLE, CHANGE IT HERE
-// //    arrayObjects[0][0].hasShip = "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif";
-// REPLACE WITH [X]AND[Y]
-        
+        var image = 'https://media3.giphy.com/media/xT0GqcCJJJH12hJvGM/giphy.gif';
+        if(arrayObjects[y][x].hasShip) {
+          image = arrayObjects[y][x].hasShip;
+        }
+            board += "<td "+ "class='board'" +
+            " data-col='"+ y + "'"+
+            " data-row='"+ x + "'>" +
+               " <img src='" +
+               image +
+               "' /></td>";
     }
     board += "</tr>";
 }
 board += "</table>";
-console.log(board)
+$("#container").html(board);
+}
+database.ref().on("value", function(newSnap){
+    newSnap.val();
+var arrayObjects = newSnap.val();
 
-$("body").append(board)
+renderBoard();
+})
+// renderBoard();
+$(document).ready(function() {
+    // renderBoard();
 
+    $("td").on("click", function() {
+        // On Click, captures the coordinates of the tile clicked.
+    var col = $(this).attr("data-row");
+    console.log(col);
+    var row = $(this).attr("data-col");
+    console.log(row);
+    var ship = "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif"
+    // Changes the value of that specific part of the array
+    arrayObjects[row][col].hasShip=ship;
+    // sends changes to firebase.
+    database.ref().set(arrayObjects);
+    
 
-$("img").on("click", function(event){
-
-	arrayObjects[0][0].hasShip = "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif";
-
-	database.ref().set(arrayObjects);
-
-	database.ref().on("value", function(snap){
-		console.log()
-	})
+    
 });
+})
 
+// arrayObjects[0][0].hasShip = "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif";
+
+    // database.ref().set(arrayObjects);
+
+    // database.ref().on("value", function(snap){
+    //  console.log()
+    // })
+// EVERY TIME THE USER CLICKS ON AN IMAGE, STORE THE VALUE ON A VARIABLE, CHANGE IT HERE
+// //    arrayObjects[0][0].hasShip = "https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif";
+// REPLACE WITH [X]AND[Y]
+
+// Boat gif image https://media0.giphy.com/media/3oz8xRQiRlaS1XwnPW/giphy.gif
+// water gif https://media3.giphy.com/media/xT0GqcCJJJH12hJvGM/giphy.gif
 
 
 
